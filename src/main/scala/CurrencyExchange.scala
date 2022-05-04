c98c3989a1f4955a6d5181e314d463ded66ef027
Jacek Laskowski <jacek@japila.pl>
2022-05-04 09:39:12 +0200

    Tiny changes

--- src/main/scala/CurrencyExchange.scala
+++ src/main/scala/CurrencyExchange.scala
@@ -1,9 +1,3 @@
-import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.kstream.Printed
-
-import java.util.Properties
-
 object CurrencyExchange extends App {
 
   import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
@@ -15,15 +9,19 @@ object CurrencyExchange extends App {
 
   import org.apache.kafka.streams.StreamsConfig
 
+  import java.util.Properties
   val props = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-table-inner-join")
-  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  val appId = CurrencyExchange.getClass.getSimpleName.replace("$", "")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, appId)
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, ":9092")
+  import org.apache.kafka.common.serialization.Serdes
   props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
   props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
 
   val amounts: KStream[String, String] = builder.stream[String, String]("amounts")
   val rates: KTable[String, String] = builder.table[String, String]("rates")
 
+  import org.apache.kafka.streams.kstream.Printed
   amounts.print(Printed.toSysOut[String,String].withLabel("[Amounts]"))
 
   val solution = amounts.join(rates) { (amt: String, rate: String) =>
@@ -32,9 +30,10 @@ object CurrencyExchange extends App {
 
   solution.to("out")
 
-  val topology = builder.build
+  val topology = builder.build()
   topology.describe()
 
+  import org.apache.kafka.streams.KafkaStreams
   val ks = new KafkaStreams(topology, props)
   ks.start()
 
